58d9390583c959efc904fad05ce3738367fb046a
AndreyRD <alonsorojasdelga@hotmail.com>
2023-12-03 10:54:07 -0600

    feat: Enabled choice for pull_rebase (#10)

--- Services/Inputs.py
+++ Services/Inputs.py
@@ -6,8 +6,9 @@ def inputs():
      name = input("Insert the profile name: ")
      user_name = input("Insert you user name: ")
      email = input("Inert you user email: ")
-     pull_rebase = input("Do you want to enable pull rebase? Enter 'true' to enable and 'false' to disable.: ")
+     pull_rebase = input("Do you want to enable pull rebase y/n?: ")
+     rebase = 'true' if pull_rebase == "y" else 'false'
      list_keys()
      ssh_key_name = input("Enter the name of your SSH key.: ")
-     profile = models.Profile(user_name = user_name, name = name, email = email, pull_rebase = pull_rebase, ssh_key_name = ssh_key_name)
+     profile = models.Profile(user_name = user_name, name = name, email = email, pull_rebase = rebase , ssh_key_name = ssh_key_name)
      return profile
